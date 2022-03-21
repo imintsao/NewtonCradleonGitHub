357474ba7b4a94370bf993d2e954358fb9641adf
imintsao <s3104029@gmail.com>
2022-03-21 14:44:30 +0800

    Beam's rotation is still need to be done

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,46 +1,69 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
-    int beamsCount = 2;
+//public class GameManager : MonoBehaviour
+//{
+//    GameObject rightBeam;
+//    GameObject leftBeam;
+//    float zGap = 4f;
+//    int duoRodTotal;
 
+//    GameObject[] rodArray;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        CreateBeams(beamsCount);
-        
-        
-    }
+//    void Start()
+//    {
+//        CreateMainBeam();
+//        CreateMainBeam(zGap);
+//        duoRodTotal = 2;
 
-
-    private void CreateBeams(int totalBeams)
-    {
-        GameObject[] beamArray;
-        beamArray = new GameObject[beamsCount];
-        float zGap = 4.0f;
-        float xScale = 4.0f;
+//        rodArray = new GameObject[duoRodTotal];
+//        for (int i = 0; i < duoRodTotal; i++)
+//        {
+//            rodArray[i] = SetRod();
 
 
-        for (int i =0; i<totalBeams; i++)
-        {
-            beamArray[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            beamArray[i].transform.position = new Vector3(0f, 0f, zGap*i);
-            beamArray[i].transform.localScale = new Vector3(xScale, 1f, 1f);
+//        }
 
+//    }
 
-    
-        }
+//    private void CreateMainBeam()
+//    {
+//        rightBeam = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+//        rightBeam.transform.Rotate(0f, 0f, 90.0f);
 
-        
-    }
+//    }
+//    private void CreateMainBeam(float zDistance)
+//    {
+//        leftBeam = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+//        leftBeam.transform.Rotate(0f, 0f, 90.0f);
+//        leftBeam.transform.position = new Vector3(0f, 0f, zDistance);
+//    }
 
-    
-    void Update()
-    {
-        
-    }
-}
+//    private GameObject[] SetRod()
+//    {
+//        //GameObject refDuoRod;
+//        GameObject[] refRod;
+//        int rodCount = 2;
+//        refRod = new GameObject[rodCount];
+//        float yGap;
+//        yGap = leftBeam.transform.position.y - leftBeam.transform.localScale.x / 2;
+
+//        Rigidbody rodRB;
+//        Rigidbody rightBeamRB;
+//        Rigidbody leftBeamRB;
+//        HingeJoint rodHJ;
+
+//        for (int i = 0; i < rodCount; i++)
+//        {
+//            refRod[i] = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+//            refRod[i].transform.position = new Vector3(0f, yGap - refRod[i].transform.localScale.y, zGap * i);
+//            rodHJ = refRod[i].AddComponent<HingeJoint>();
+//            //return refRod[i];
+
+//        }
+
+//        return refRod;
+
+//    }
+//}
---
+++ Assets/Scripts/NewGameManager.cs
@@ -0,0 +1,135 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewGameManager : MonoBehaviour
+{
+    GameObject leftBeam;
+    GameObject rightBeam;
+    float zGap = 2.6f;
+    float yGap;
+    GameObject[] sphereArray;
+
+    Rigidbody beamRB;
+
+    float tempRadius;
+
+
+    int cradleCount;
+
+
+
+    struct TwinRods
+    {
+        public float setNumber;
+        public GameObject lRod;
+        public GameObject rRod;
+
+    }
+
+    private TwinRods SetRods(float setNum)
+    {
+        GameObject lRod, rRod;
+        TwinRods refRods;
+        //float yGap;
+        yGap = leftBeam.transform.position.y - leftBeam.transform.localScale.x / 2;
+        HingeJoint rodHJ;
+        Rigidbody rodRB;
+
+        lRod = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        lRod.name = "LeftRod";
+        rRod = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        rRod.name = "RightRod";
+
+        lRod.transform.localScale = new Vector3(0.1f, 1.5f, 0.1f);
+        lRod.transform.position = new Vector3(0f, yGap - lRod.transform.localScale.y, 0f);
+        rodHJ=lRod.AddComponent<HingeJoint>();
+        rodHJ.connectedBody = beamRB;
+        rodHJ.axis = new Vector3(0f, 0f, 1f);
+        lRod.transform.parent = leftBeam.transform;
+
+        rodRB = lRod.GetComponent<Rigidbody>();
+        rodRB.useGravity = (true);
+        rodRB.isKinematic = (false);
+
+
+        rRod.transform.localScale = new Vector3(0.1f, 1.5f, 0.1f);
+        rRod.transform.position = new Vector3(0f, yGap - rRod.transform.localScale.y, zGap);
+        rRod.AddComponent<HingeJoint>();
+
+
+
+        refRods.lRod = lRod;
+        refRods.rRod = rRod;
+        refRods.setNumber = setNum;
+
+        return refRods;
+
+    }
+
+    private void Start()
+    {
+
+        CreateMainBeam();
+        CreateMainBeam(zGap);
+
+        TwinRods[] rodArray;
+
+        cradleCount = 1;
+
+
+        rodArray = new TwinRods[cradleCount];
+
+        tempRadius = 0.5f;
+        sphereArray = new GameObject[cradleCount];
+
+        for (int i = 0; i < cradleCount; i++)
+        {
+            rodArray[i] = SetRods(i);
+            //sphereArray[i] = SetSphere();
+
+        }
+    }
+
+    private void CreateMainBeam()
+    {
+        leftBeam = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        leftBeam.transform.Rotate(0f, 0f, 90.0f);
+
+        beamRB = leftBeam.AddComponent<Rigidbody>();
+        beamRB.useGravity = (false);
+        beamRB.isKinematic = (true);
+        leftBeam.name = "LeftBeam";
+
+        leftBeam.transform.rotation = Quaternion.Euler(-30f, 0f, 0f);
+        //leftBeam.transform.Rotate(-30f, 0f, 0f, Space.World);
+        //rightBeam.transform.position=new Vector3(0f,)
+
+    }
+    private void CreateMainBeam(float zDistance)
+    {
+        rightBeam = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        rightBeam.transform.Rotate(0f, 0f, 90.0f);
+        rightBeam.transform.position = new Vector3(0f, 0f, zDistance);
+        rightBeam.name = "RightBeam";
+        
+    }
+
+    
+
+    //private GameObject SetSphere()
+    //{
+    //    GameObject refBall;
+    //    float yGap;
+    //    //refYGap= leftBeam.transform.position
+
+    //    refBall = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+    //    refBall.name = "Cradle";
+
+
+
+    //    return refBall;
+    //}
+
+   
+}
